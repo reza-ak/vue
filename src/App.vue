41622451fbc5fc87334d53762b751a6fea73ecc8
reza-ak <reeza.dev@gmail.com>
2023-05-20 20:27:49 +0430

    import bootstrap to project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>index</h1>
+  <h1 class="text-info">index</h1>
   <router-view></router-view>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
+// import bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.rtl.min.css' // در بوت استرپ 5 rtl هم داریم
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
