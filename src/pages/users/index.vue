974c93026d94c54f8ed165303315b4a5bfbee09a
reza-ak <reeza.dev@gmail.com>
2023-05-21 13:46:43 +0430

    installing axios package and get users list with API

---
+++ src/pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>users</h1>
+</template>
+
+<script setup>
+import axios from "axios";
+function getUsers() {
+  axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
+getUsers()
+</script>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/Home.vue"
+import IndexUsers from "./pages/users/index.vue"
 
 const routes = [
-  {path: "/", name:"home", component: Home}
+  {path: "/", name:"home", component: Home},
+  {path: "/users", name:"users", component: IndexUsers}
 ];
 
 const router = createRouter({
