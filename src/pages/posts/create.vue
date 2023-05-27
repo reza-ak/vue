9b7ff330e2a9744e5a6a63e5e3b3f210514ac9ad
reza-ak <reeza.dev@gmail.com>
2023-05-27 12:57:35 +0430

    Install the sweetalert2 package and use it to display the post creation message

--- src/pages/posts/create.vue
+++ src/pages/posts/create.vue
@@ -37,6 +37,7 @@
 <script setup>
 import axios from "axios";
 import { reactive, ref } from "vue";
+import Swal from 'sweetalert2'
 
 const formData = reactive({
   title: "",
@@ -74,6 +75,12 @@ function create() {
       // handle success
       console.log(response.data);
       loading.value = false;
+      Swal.fire({
+      title: 'Thanks!',
+      text: 'Post created succesfully',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    })
     })
     .catch(function (error) {
       // handle error
